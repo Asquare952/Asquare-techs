6820ba64ca89d5df22a0f24921e50b7d2888c6e5
Asquare2003 <abdulafeezabdulazeez894@gmail.com>
2025-02-11 17:59:43 +0100

    fix bug

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,28 @@
+'use client'
+
 import Link from "next/link"
 import Image from "next/image"
 import { FaArrowLeft } from "react-icons/fa6";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 const Page = () => {
+  const {
+      login,
+      handleSubmit,
+      formState: { errors },
+    } = useForm({ resolver: yupResolver(schema) });
+  
+    const onSubmit = (data) => console.log("User Data:", data);
   return (
     <section className="flex items-center gap-32 mt-14">
       <div className="bg-blueShades-200 hidden 2xl:block">
@@ -21,25 +41,35 @@ const Page = () => {
           <h3 className=" font-medium text-4xl ">Log in to Asquarez Tech</h3>
           <p className="font-normal text-base">Enter your details below</p>
         </div>
-        <form className="flex flex-col gap-10">
+        <form
+          className="flex flex-col gap-10"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
             type="email"
             placeholder="Email"
             className="form-input outline-none pb-4"
+            {...login("email")}
           />
+          {errors.email && (
+            <p className="text-red-800">{errors.email.message}</p>
+          )}
+
           <input
             type="password"
             placeholder="Password"
             className="form-input outline-none pb-4"
+            {...login("password")}
           />
-        </form>
-        <div className="flex items-center gap-20">
+
           <button
             type="submit"
             className="text-center bg-redShades-200 rounded-md py-4 px-12 text-greyShades-300"
           >
             Log in
           </button>
+        </form>
+        <div className="flex items-center gap-20">
           <Link href="#" className="text-redShades-200">
             Forget Password?
           </Link>
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,7 +1,31 @@
+'use client'
+
 import Image from "next/image";
 import { FaArrowLeft } from "react-icons/fa6";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+// import { Formik } from "formik";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
+
 const page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const onSubmit = (data) => console.log("User Data:", data);
+
   return (
     <section className="flex items-center gap-32 mt-14">
       <div className="bg-blueShades-200 hidden 2xl:block">
@@ -20,30 +44,43 @@ const page = () => {
           <h3 className=" font-medium text-4xl ">Create an account</h3>
           <p className="font-normal text-base">Enter your details below</p>
         </div>
-        <form className="flex flex-col gap-10">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-10"
+          noValidate
+        >
           <input
-            type="text"
+            {...register("name")}
             placeholder="Name"
             className="form-input outline-none pb-4"
           />
+          {errors.name && <p className="text-red-800">{errors.name.message}</p>}
           <input
-            type="email"
+            {...register("email")}
             placeholder="Email"
             className="form-input outline-none pb-4"
           />
+          {errors.email && (
+            <p className="text-red-800">{errors.email.message}</p>
+          )}
           <input
+            {...register("password")}
             type="password"
             placeholder="Password"
             className="form-input outline-none pb-4"
           />
-        </form>
-        <div className="flex flex-col items-center gap-6">
+          {errors.password && (
+            <p className="text-red-800">{errors.password.message}</p>
+          )}
           <button
             type="submit"
             className="text-center bg-redShades-200 rounded-md py-4 px-28 text-greyShades-300"
           >
             Create account
           </button>
+        </form>
+
+        <div className="flex flex-col items-center gap-6">
           <p>
             Already have account?{" "}
             <a href="/login" className="">
